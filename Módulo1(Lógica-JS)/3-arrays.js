1096f14315f8b4e25c19767e9f753fa65fd41773
Hugo <hugoborrego02@gmail.com>
2025-05-02 19:12:33 -0300

    aula7

--- Módulo1(Lógica-JS)/3-arrays.js
+++ Módulo1(Lógica-JS)/3-arrays.js
@@ -1,3 +1,9 @@
+let listaNomes = ['Arthur', 'Bruno', 'Carlos', 'Daniel', 'Edurardo']
+
+for (let i = 0; i < listaNomes.length; i++) {
+    console.log(listaNomes[i])  
+}
+
 let listaFrutas = Array()
 listaFrutas[0] = 'Abacate' // Adicionando no array de indice 0
 listaFrutas[1] = 'Banana' // Adicionando no array de indice 1
@@ -8,9 +14,8 @@ listaFrutas.push('pop')
 listaFrutas.pop() // Excluindo o último do array
 listaFrutas.unshift() // Excluindo no começo do array
 
-
 for (let i = 0; i < listaFrutas.length; i++) {
     console.log(listaFrutas[i])  
 }
 
-console.table(listaFrutas)
+console.table(listaFrutas) // Mostra uma tabela com todos os elementos
