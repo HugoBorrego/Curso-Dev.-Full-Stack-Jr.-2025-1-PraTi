c9320da872aef9b784bb0584e188e59545f2e918
Hugo <hugoborrego02@gmail.com>
2025-05-07 14:06:34 -0300

    1

--- Módulo1(Lógica-JS)/3-arrays.js
+++ Módulo1(Lógica-JS)/3-arrays.js
@@ -1,3 +1,5 @@
+const prompt = require("prompt-sync")
+
 let listaNomes = ['Arthur', 'Bruno', 'Carlos', 'Daniel', 'Edurardo']
 
 for (let i = 0; i < listaNomes.length; i++) { console.log(listaNomes[i]) }
@@ -13,3 +15,66 @@ listaFrutas.pop() // Excluindo o último do array
 listaFrutas.unshift() // Excluindo no começo do array
 
 console.table(listaFrutas) // Mostra uma tabela com todos os elementos
+
+
+// Crie um array chamado 'numeros' contendo valores numéricos a sua escolha
+let numeros = [1, 2, 8, 4, 9, 6, 7, 3, 5, 10]
+// Use métodos de array para calcular:
+// A soma de todos elementos
+ 
+let soma = numeros.reduce((acumulador, valor) => acumulador + valor, 0)
+// MÉTODO NORMAL for (let i = 0; i < numeros.length; i++) { soma += numeros[i] }
+
+
+// Média dos elementos
+let media = 0
+media = soma / numeros.length
+
+// Gere um novo array chamado pares que contenha apenas os números pares do array numeros
+let pares = numeros.filter((num => num % 2 == 0))
+// MÉTODO NORMAL for (let i = 0; i < numeros.length; i++) { if (numeros[i] % 2 == 0) { pares.push(numeros[i]) } }
+
+console.log(soma)
+console.log(media)
+console.log(pares)
+
+// Ordene um array numeros em ordem crescente
+let ordenado = numeros.sort((a, b) => a - b)
+console.log(numeros)
+
+
+// MÉTODO BubbleSort 
+let valores = [3, 1, -4, 6, -1]
+for (let i = 0; i < valores.length; i++) {
+    for (let j = 0; j < valores.length - i; j++) {
+        if (valores[j] > valores[j + 1]) {
+            let temp = valores[j]
+            valores[j] = valores[j + 1]
+            valores[j + 1] = temp
+        }
+    }
+}
+console.log(valores)
+
+// Matrizes
+
+let matriz = []
+let somar = 0
+for (let i = 0; i < 3; i++) {
+    matriz[i] = []
+    for (let j = 0; j < 3; j++) {
+        matriz[i][j] = 7
+        // Calculo da diagonal da matriz
+        if (i == j) { 
+            somar += matriz[i][j] 
+        }
+    }
+}
+console.table(matriz)
+console.log(somar)
+
+
+let mercado = []
+mercado['Fruteira'] = ['Maça', 'Pera', 'Abacaxi']
+mercado['Açougue'] = ['Picanha', 'Alcatra', 'Filé']
+console.table(mercado)
--- Módulo1(Lógica-JS)/tempCodeRunnerFile.js
+++ Módulo1(Lógica-JS)/tempCodeRunnerFile.js
@@ -1,25 +1,17 @@
-const readline = require('readline-sync');
-
-function mostrarMenu() {
-    console.log("-- MENU --");
-    console.log("1. Opção 1");
-    console.log("2. Opção 2");
-    console.log("3. Opção 3");
-
-    let opcao = readline.question("Escolha uma opcao: ");
-
-    switch (opcao) {
-        case "1":
-            console.log("Você escolheu a Opção 1!");
-            break;
-        case "2":
-            console.log("Você escolheu a Opção 2!");
-            break;
-        case "3":
-            console.log("Você escolheu a Opção 3!");
-            break;
-        default:
-            console.log("Opção inválida!");
+// Matriz
+let matriz = []
+let somar = 0
+for (let i = 0; i < 3; i++) {
+    matriz[i] = []
+    for (let j = 0; j < 3; j++) {
+        matriz[i][j] = 7
+        if (i == j) {
+            somar += matriz[i][j]
+        }
     }
 }
-mostrarMenu();
+console.table(matriz)
+// Calculo da diagonal da matriz
+
+
+console.log(somar)
