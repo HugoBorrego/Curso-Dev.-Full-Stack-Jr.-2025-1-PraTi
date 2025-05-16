290bdd882049fad6f026c87c048868381d9076a5
Hugo <hugo.borrego02@gmail.com>
2025-05-16 14:33:59 -0300

    recursividade

--- Módulo1(Lógica-JS)/6-objetos.js
+++ Módulo1(Lógica-JS)/6-objetos.js
@@ -124,3 +124,51 @@ filmes.forEach((filme) => {
 })
 
 console.table(contagemFilmes)
+
+// Função Recursiva
+function fatorial(n) {
+    if (n == 0) {
+        return 1
+    } else {
+        return n * fatorial(n-1)
+    }
+}
+console.log(fatorial(5))
+
+function somatorio(num) {
+    if (num == 1) {
+        return 1
+    } else {
+        return num + somatorio(num-1)
+    }
+}
+console.log(somatorio(5))
+
+let categorias = [
+    {
+        id: 1,
+        nome: 'Eletrônicos',
+        filhos: [ 
+            { id: 2, nome: 'Celulares', filhos: [] },
+            { id: 3, nome: 'Computadores', filhos: [
+                {id: 4, nome: 'Tablets', filhos: []}
+            ] }
+        ]},
+    {
+        id: 5,
+        nome: 'Louças',
+        filhos: []
+    }
+]
+
+// Função recursiva para imprimir somente os nomes
+function imprimirCategorias(lista, nivel = 0) {
+    for (let categoria of lista) {
+        console.log(' '.repeat(nivel * 2) + categoria.nome)
+        if (categoria.filhos.length > 0) {
+            imprimirCategorias(categoria.filhos, nivel + 1)
+        }
+    }
+}
+
+imprimirCategorias(categorias)
--- Módulo1(Lógica-JS)/tempCodeRunnerFile.js
+++ Módulo1(Lógica-JS)/tempCodeRunnerFile.js
@@ -1,28 +1,28 @@
-let filmes = [
-    {nome: 'Deadpool', genero: 'Ação'},
-    {nome: 'John Wick', genero: 'Ação'},
-    {nome: 'Mad Max: Estrada da Fúria', genero: 'Ação'},
-    {nome: 'Logan', genero: 'Drama'},
-    {nome: 'Blade Runner 2049', genero: 'Ficção Científica'},
-    {nome: 'O Exterminador do Futuro 2', genero: 'Ficção Científica'},
-    {nome: 'Aliens, O Resgate', genero: 'Ficção Científica'},
-    {nome: 'Halloween', genero: 'Terror'},
-    {nome: 'O Iluminado', genero: 'Terror'},
-    {nome: 'Clube da Luta', genero: 'Suspense'},
-    {nome: 'O Poderoso Chefão', genero: 'Drama'},
-    {nome: 'Batman: O Cavaleiro das Trevas', genero: 'Crime'},
-    {nome: 'Senhor dos Anéis: O Retorno do Rei', genero: 'Fantasia'},
-    {nome: 'Harry Potter e o Prisioneiro de Azkaban', genero: 'Fantasia'},
-    {nome: 'La La Land', genero: 'Romance'}
-];
-
-let contagemFilmes = {}
-filmes.forEach((filme) => {
-    if (contagemFilmes[filme.genero]) {
-        contagemFilmes[filme.genero]++
-    } else {
-        contagemFilmes[filme.genero] = 1
+let categorias = [
+    {
+        id: 1,
+        nome: 'Eletrônicos',
+        filhos: [ 
+            { id: 2, nome: 'Celulares', filhos: [] },
+            { id: 3, nome: 'Computadores', filhos: [
+                {id: 4, nome: 'Tablets', filhos: []}
+            ] }
+        ]},
+    {
+        id: 5,
+        nome: 'Louças',
+        filhos: []
     }
-})
+]
 
-console.table(contagemFilmes)
+// Função recursiva para imprimir somente os nomes
+function imprimirCategorias(lista, nivel = 0) {
+    for (let categoria of lista) {
+        console.log(' '.repeat(nivel * 2) + categoria.nome)
+        if (categoria.filhos.length > 0) {
+            imprimirCategorias(categoria.filhos, nivel + 1)
+        }
+    }
+}
+
+imprimirCategorias(categorias)
