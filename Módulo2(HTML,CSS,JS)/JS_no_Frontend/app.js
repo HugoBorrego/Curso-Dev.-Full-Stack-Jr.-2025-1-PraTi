a12e3c92ef89d84e6d58e2ed0c0edccb6865dcad
Hugo <hugo.borrego02@gmail.com>
2025-07-03 22:13:07 -0300

    Entendendo DOM e BOM

--- Módulo2(HTML,CSS,JS)/JS_no_Frontend/app.js
+++ Módulo2(HTML,CSS,JS)/JS_no_Frontend/app.js
@@ -63,3 +63,10 @@ btnLimpar.addEventListener('click', () => {
         removerItem()
     }
 })
+
+// Funcionalidades:
+
+// Marcar como comprado - Salvar esse Estado no localStorage
+// Contador de Itens - Mostrar quantos itens tem na lista, atualizando em tempo real
+// Adicione filtros para itens 'comprados' e 'pedentes'
+// Permita ordenar alfabeticamente ou por status
