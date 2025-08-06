ae05003ac9a4e0eaa1533a8e8ce2c7447d4c67ba
Hugo <hugo.borrego02@gmail.com>
2025-08-06 12:10:11 -0300

    Requisição HTTP usando Axios

--- Modulo3-ReactJS/5-Requests/src/main.jsx
+++ Modulo3-ReactJS/5-Requests/src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import FetchGet from './components/FetchUser'
+import AxiosGet from './components/AxiosUser'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <FetchGet />
+    <AxiosGet />
   </StrictMode>,
 )
