b10e342eca7ff3c6645ed5aa77e37ed7034110dd
Hugo <hugo.borrego02@gmail.com>
2025-08-19 13:58:43 -0300

    Estilização com ReactJS

---
+++ Modulo3-ReactJS/8-styles/src/App.jsx
@@ -0,0 +1,28 @@
+import './styles/base.css'
+import './styles/utilites.css'
+import './styles/components.css'
+
+import { Button } from './components/Button/button'
+import { Card } from './components/Card/card'
+
+import CardStyled from './components/Card/CardStyled'
+
+
+function App() {
+  
+
+  return (
+    <>
+    <Card title='Aula de Estilização'>
+      <main className='container'>
+        <p>Estilos provenientes de arquivos globais.</p>
+        <Button>Enviar</Button>
+        </main>
+      </Card>
+
+      <CardStyled />
+    </>
+  )
+}
+
+export default App
