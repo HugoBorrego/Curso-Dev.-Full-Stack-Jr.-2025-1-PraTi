5a2a8e299869d4fcedaf4a263c214c9fc2b2d4da
Hugo <hugo.borrego02@gmail.com>
2025-09-10 11:02:59 -0300

    Atualizando os projetos e pastas

--- Modulo4-Java/opp/src/InterfaceExamples/ExercicioNotificacao/EmailNotificador.java
+++
@@ -1,8 +0,0 @@
-package InterfaceExamples.ExercicioNotificacao;
-
-public class EmailNotificador implements EnviadorNotificacao {
-    @Override
-    public void enviar(String destino, String mensagem) {
-        System.out.println("E-mail para: "+ destino + ": " + mensagem);
-    }
-}
--- Modulo4-Java/opp/src/InterfaceExamples/ExercicioNotificacao/SmsNotificador.java
+++
@@ -1,8 +0,0 @@
-package InterfaceExamples.ExercicioNotificacao;
-
-public class SmsNotificador implements EnviadorNotificacao {
-    @Override
-    public void enviar(String destino, String mensagem) {
-        System.out.println("SMS para: "+ destino + ": " + mensagem);
-    }
-}
