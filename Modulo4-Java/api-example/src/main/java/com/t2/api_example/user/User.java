644abc151e3f51d309ddbf85af53aa4749d96032
Hugo <hugo.borrego02@gmail.com>
2025-10-13 15:25:32 -0300

    Desenvolvendo uma API e implementando autenticação com JWT

---
+++ Modulo4-Java/api-example/src/main/java/com/t2/api_example/user/User.java
@@ -0,0 +1,114 @@
+package com.t2.api_example.user;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "users", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "username")
+})
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(max = 50)
+    private String name;
+
+    @NotBlank
+    @Size(max = 50)
+    @Email(message = "E-mail está em formato inválido")
+    private String username;
+
+    @NotBlank
+    @Size(min = 60, max = 100)
+    private String password;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
+    @Column(name = "role")
+    private Set<String> roles = new HashSet<>();
+
+    private Instant createdAt;
+    private Instant updatedAt;
+
+    @PrePersist
+    public void prePersist() {
+        Instant now = Instant.now();
+        this.createdAt = now;
+        this.updatedAt = now;
+
+        if (roles == null || roles.isEmpty()) {
+            roles = new HashSet<>();
+            roles.add("USER");
+        }
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedAt = Instant.now();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public @NotBlank @Size(max = 50) String getName() {
+        return name;
+    }
+
+    public void setName(@NotBlank @Size(max = 50) String name) {
+        this.name = name;
+    }
+
+    public @NotBlank @Size(max = 50) @Email String getUsername() {
+        return username;
+    }
+
+    public void setUsername(@NotBlank @Size(max = 50) @Email String username) {
+        this.username = username;
+    }
+
+    public @NotBlank @Size(min = 10, max = 100) String getPassword() {
+        return password;
+    }
+
+    public void setPassword(@NotBlank @Size(min = 10, max = 100) String password) {
+        this.password = password;
+    }
+
+    public Set<String> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<String> roles) {
+        this.roles = roles;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Instant updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
